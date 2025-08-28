51248231b2f0ac8dda305fcfb8cef5af67990b22
Feisel1 <feimah081007@student.jenseneducation.se>
2025-08-28 09:48:08 +0200

    klar

--- uppg1_greetings.py
+++ uppg1_greetings.py
@@ -7,3 +7,12 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Hej (Ulrika)!
 Oj vad du är gammal.
 """
+
+namn = input("vad heter du? ")
+ålder = int(input("hur gammal är du? "))
+if ålder <=19:
+    print(f"Hej {namn}! Oj vad du är ung, du får inte komma in i hemsidan.")
+elif 20 <= ålder <= 50:
+    print(f"Hej {namn}! Välkommen till hemsidan.")
+else:
+    print(f"Hej {namn}! Oj vad du är gammal, men du är fortfarande välkomen.")
