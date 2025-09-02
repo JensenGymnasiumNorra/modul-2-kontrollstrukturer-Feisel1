f9acd7aee16257d02dc85646a90c910479b715b8
Feisel1 <feimah081007@student.jenseneducation.se>
2025-09-02 16:25:43 +0200

    klar

--- uppg2_for_loops.py
+++ uppg2_for_loops.py
@@ -5,3 +5,24 @@ Skriv ut alla tal mellan 13 och 37.
 Skriv ut alla jämna tal mellan 4 och 20.
 Skriv ut alla tal mellan 20 och 0, börja med 20 och sluta på 0.
 """
+
+for i in range(10):
+    print("celsius")    
+for i in range(1):
+    print("")    
+for i in range(1,9):
+    print(i) 
+for i in range(1):
+    print("")
+for i in range(13,38,):
+    print(i)
+for i in range(1):
+    print("")
+for i in range(4,21,2):
+    print(i)
+for i in range(1):
+    print("")   
+for i in range(20, -1,-1):
+    print(i)
+ 
+
